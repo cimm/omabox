8c4fcd8fc90c5b4d4f0b6b3eb6ee4909e31ab2a6
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2020-12-05 16:45:54 +0100

    Fix JavaScript warnings with JSHint

--- uploader/src/uploader.js
+++ uploader/src/uploader.js
@@ -1,12 +1,14 @@
+/* jshint esversion: 8 */
+
 // Resizes the image to 800px (screen with of Raspberry Pi)
 // and uploads the image to the Backblaze B2 bucket specified
 // by the authorizationToken and uploadUrl.
 
 async function upload(files, authorizationToken, uploadUrl) {
-  spinner(true)
+  spinner(true);
 
-  const [ file ] = files
-  const maxSize = 800
+  const [ file ] = files;
+  const maxSize = 800;
 
   await fetch(uploadUrl, {
     method: 'POST',
@@ -17,30 +19,36 @@ async function upload(files, authorizationToken, uploadUrl) {
     },
     body: await resizeImage(file, maxSize)
   })
-  .then((response) => response.json())
-  .then((data) => {
-    spinner(false)
-    document.getElementById('fileUpload').value = ''
-    (data.action === 'upload') ? alert('Image uploaded') : alert('Oops, something went wrong')
+  .then(response => response.json())
+  .then(data => {
+    spinner(false);
+    document.getElementById('fileUpload').value = '';
+    if (data.action === 'upload') {
+      alert('Image uploaded');
+    } else {
+      alert('Oops, something went wrong');
+    }
   })
-  .catch((error) => {
-    spinner(false)
-    alert(error)
-  })
+  .catch(error => {
+    spinner(false);
+    alert(error);
+  });
 }
 
-// Semi-randomizes the filename so we don't override 
+// Semi-randomizes the filename so we don't override
 // existing files in the bucket.
 function randomizeFilename(file) {
-  return Math.random().toString(36).substring(2) + '.' + file.name.split('.').pop()
+  const prefix = Math.random().toString(36).substring(2);
+  const extension =  file.name.split('.').pop();
+  return  prefix + '.' + extension;
 }
 
 function spinner(active) {
-  let spin = document.getElementsByClassName('loader')[0]
+  let spin = document.getElementsByClassName('loader')[0];
   if (active) {
-    spin.classList.add('is-active')
+    spin.classList.add('is-active');
   } else {
-    spin.classList.remove('is-active')
+    spin.classList.remove('is-active');
   }
 }
 
@@ -48,52 +56,52 @@ function spinner(active) {
 // 1. Smaller uploads are better
 // 2. imv has issues with very wide images
 function resizeImage(file, maxSize) {
-  const reader = new FileReader()
-  const image = new Image()
-  const canvas = document.createElement('canvas')
+  const reader = new FileReader();
+  const image = new Image();
+  const canvas = document.createElement('canvas');
 
   const dataURItoBlob = function (dataURI) {
     const bytes = dataURI.split(',')[0].indexOf('base64') >= 0 ?
       atob(dataURI.split(',')[1]) :
-      unescape(dataURI.split(',')[1])
-    const mime = dataURI.split(',')[0].split(':')[1].split(';')[0]
-    const max = bytes.length
-    let ia = new Uint8Array(max)
+      unescape(dataURI.split(',')[1]);
+    const mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const max = bytes.length;
+    let ia = new Uint8Array(max);
     for (let i = 0; i < max; i++)
-      ia[i] = bytes.charCodeAt(i)
-    return new Blob([ia], { type: mime })
-  }
+      ia[i] = bytes.charCodeAt(i);
+    return new Blob([ia], { type: mime });
+  };
 
   const resize = function () {
-    let width = image.width
-    let height = image.height
+    let width = image.width;
+    let height = image.height;
     if (width > height) {
       if (width > maxSize) {
-        height *= maxSize / width
-        width = maxSize
+        height *= maxSize / width;
+        width = maxSize;
       }
     } else {
       if (height > maxSize) {
-        width *= maxSize / height
-        height = maxSize
+        width *= maxSize / height;
+        height = maxSize;
       }
     }
-    canvas.width = width
-    canvas.height = height
-    canvas.getContext('2d').drawImage(image, 0, 0, width, height)
-    const dataUrl = canvas.toDataURL('image/jpeg')
-    return dataURItoBlob(dataUrl)
-  }
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+    const dataUrl = canvas.toDataURL('image/jpeg');
+    return dataURItoBlob(dataUrl);
+  };
 
   return new Promise(function (ok, no) {
     if (!file.type.match(/image.*/)) {
-      no(alert('Not an image'))
-      return
+      no(alert('Not an image'));
+      return;
     }
     reader.onload = function (readerEvent) {
-      image.onload = function () { return ok(resize()) }
-      image.src = readerEvent.target.result
-    }
-    reader.readAsDataURL(file)
-  })
+      image.onload = function () { return ok(resize()); };
+      image.src = readerEvent.target.result;
+    };
+    reader.readAsDataURL(file);
+  });
 }
